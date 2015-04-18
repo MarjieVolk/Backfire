ad94fb20fec51f843ef215a4c94b1560c0f5e502
knexer <knexer@gmail.com>
2015-04-18 10:40:00 -0700

    Impl level population skeleton
    Consumes a pixel-art representation of the level and generates a grid.

---
+++ Assets/BulletGridGenerator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletGridGenerator : MonoBehaviour {
+
+    public GameObject CellPrefab;
+
+	// Use this for initialization
+	void Start () {
+        Vector2 center = gameObject.transform.position;
+        float realWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+        float realHeight = GetComponent<SpriteRenderer>().bounds.size.y;
+
+        Texture2D levelDescriptor = GetComponent<SpriteRenderer>().sprite.texture;
+        float cellsWidth = levelDescriptor.width;
+        float cellsHeight = levelDescriptor.height;
+
+        Vector2 botLeft = center - new Vector2(realWidth / 2, realHeight / 2);
+        float cellWidth = realWidth / cellsWidth;
+        float cellHeight = realHeight / cellsHeight;
+
+        for (int x = 0; x < cellsWidth; x++)
+        {
+            for (int y = 0; y < cellsHeight; y++)
+            {
+                Vector2 cellCenter = botLeft + new Vector2(cellWidth * x, cellHeight * y);
+
+                GameObject cell = (GameObject)Instantiate(CellPrefab, cellCenter, Quaternion.identity);
+            }
+        }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+	
+	}
+}
