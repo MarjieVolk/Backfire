e2e98ad0ea84605fff221e34805157c9d849dff6
knexer <knexer@gmail.com>
2015-04-18 16:23:35 -0700

    Partial impl of explosions
    Needs moar refactor

--- Assets/CellFeatures/ExplodeyThing.cs
+++ Assets/CellFeatures/ExplodeyThing.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class ExplodeyThing : CellFeature
 {
+    private GridPosition[] _adjacentOffsets = new GridPosition[4] { new GridPosition(1, 0), new GridPosition(0, 1), new GridPosition(-1, 0), new GridPosition(0, -1) };
+    private BulletGridGenerator _grid;
+
     public Sprite DangerZoneSprite;
     public int DangerZoneThreshold;
     public override int Amount
@@ -25,7 +28,7 @@ public class ExplodeyThing : CellFeature
 
 	// Use this for initialization
 	void Start () {
-	
+        _grid = FindObjectOfType<BulletGridGenerator>();
 	}
 	
 	// Update is called once per frame
@@ -36,7 +39,37 @@ public class ExplodeyThing : CellFeature
     void explode()
     {
         Cell cell = GetComponent<Cell>();
-        // TODO get cell position
-        // TODO explode things here (and adjacent?), maybe damage terrain, certainly kill some nanobots
+        GridPosition cellPosition = cell.GridPosition;
+        explodeNanobots(cellPosition);
+        explodeTerrain(cellPosition);
+    }
+
+    void explodeNanobots(GridPosition position)
+    {
+        //TODO how do I kill the nanobots?
+        GameObject nanobotToKill = _grid.getCellAt(position).Nanobot;
+        foreach (GridPosition offset in _adjacentOffsets){
+            GridPosition adjacent = _grid.applyDelta(position, offset);
+            if (adjacent != null)
+            {
+                GameObject adjacentNanobotToKill = _grid.getCellAt(adjacent).Nanobot;
+                // TODO kill this guy too
+            }
+        }
+    }
+
+    void explodeTerrain(GridPosition position)
+    {
+        //TODO how do I blow up terrain w/o giving the player resources? Answer: I can't b/c I implemented it dumbly
+        GameObject terrainToExplode = _grid.getCellAt(position).Cell;
+        foreach (GridPosition offset in _adjacentOffsets)
+        {
+            GridPosition adjacent = _grid.applyDelta(position, offset);
+            if (adjacent != null)
+            {
+                GameObject adjacentTerrainToExplode = _grid.getCellAt(adjacent).Cell;
+                // TODO kill this guy too
+            }
+        }
     }
 }
