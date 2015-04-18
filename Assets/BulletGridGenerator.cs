8a67609b62283ce14ffa50059fb992110d78908a
knexer <knexer@gmail.com>
2015-04-18 11:14:18 -0700

    Flesh out level population
    Now we need only add support for new cell types as we create them

--- Assets/BulletGridGenerator.cs
+++ Assets/BulletGridGenerator.cs
@@ -1,20 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class BulletGridGenerator : MonoBehaviour {
 
-    public GameObject CellPrefab;
+    public GameObject NormalCell;
+
+    public GameCell[][] GameGrid;
 
 	// Use this for initialization
 	void Start () {
+        // Set up bullet grid based on levelDescriptor's size
+        Texture2D levelDescriptor = GetComponent<SpriteRenderer>().sprite.texture;
+        int cellsWidth = levelDescriptor.width;
+        int cellsHeight = levelDescriptor.height;
+        GameGrid = new GameCell[cellsWidth][];
+        for (int x = 0; x < cellsWidth; x++)
+        {
+            GameGrid[x] = new GameCell[cellsHeight];
+        }
+
+        // In-world grid size is based on this GameObject's size
         Vector2 center = gameObject.transform.position;
         float realWidth = GetComponent<SpriteRenderer>().bounds.size.x;
         float realHeight = GetComponent<SpriteRenderer>().bounds.size.y;
 
-        Texture2D levelDescriptor = GetComponent<SpriteRenderer>().sprite.texture;
-        float cellsWidth = levelDescriptor.width;
-        float cellsHeight = levelDescriptor.height;
-
+        // Populate bullet grid
         Vector2 botLeft = center - new Vector2(realWidth / 2, realHeight / 2);
         float cellWidth = realWidth / cellsWidth;
         float cellHeight = realHeight / cellsHeight;
@@ -23,16 +34,37 @@ public class BulletGridGenerator : MonoBehaviour {
         {
             for (int y = 0; y < cellsHeight; y++)
             {
-                Vector2 cellCenter = botLeft + new Vector2(cellWidth * x, cellHeight * y);
+                Vector2 cellCenter = botLeft + new Vector2(cellWidth * (x + 0.5f), cellHeight * (y + 0.5f));
 
-                GameObject cell = (GameObject)Instantiate(CellPrefab, cellCenter, Quaternion.identity);
+                //pick the cell type to place based on the color of the pixel at this location
+                GameObject cellPrefab = getPrefabForColor(levelDescriptor.GetPixel(x, y));
+                GameObject cell = (GameObject)Instantiate(cellPrefab, cellCenter, Quaternion.identity);
+                GameGrid[x][y] = new GameCell();
+                GameGrid[x][y].Cell = cell;
             }
         }
 	}
+
+    private GameObject getPrefabForColor(Color color)
+    {
+        Color grey = new Color(127/255.0f, 127/255.0f, 127/255.0f);
+        if (color.Equals(grey)) // grey
+        {
+            return NormalCell;
+        }
+        Debug.Log(color);
+        Debug.Log(grey);
+        throw new ArgumentException();
+    }
 
     // Update is called once per frame
     void Update()
     {
 	
 	}
+
+    public class GameCell {
+        public GameObject Cell;
+        public GameObject Nanobot;
+    }
 }
