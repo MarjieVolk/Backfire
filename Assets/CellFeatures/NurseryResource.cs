84c3ed7634da58b6097db024c40503f093bb8f2a
knexer <knexer@gmail.com>
2015-04-18 12:43:48 -0700

    Sketch impl of cell resources
    Nanobots can Eat a Cell, which will consume the CellFeatures on the
    Cell, each of which does a different thing when eaten, in order from top
    to bottom.

---
+++ Assets/CellFeatures/NurseryResource.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NurseryResource : CellFeature {
+
+    public override int Amount
+    {
+        set
+        {
+            // add _amount - value to the player's resource store
+            _amount = value;
+            Debug.Log("child thingy");
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
