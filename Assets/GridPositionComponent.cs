e68713a87311ec9278997685bcd03d3421876b91
Marjie Volk <marjie.volk@gmail.com>
2015-04-18 18:42:16 -0700

    Fixed nanobot placement bug

--- Assets/GridPositionComponent.cs
+++ Assets/GridPositionComponent.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class GridPositionComponent : MonoBehaviour {
 
-    public GridPosition position;
+    public GridPosition position = new GridPosition(0, 0);
 
 	// Use this for initialization
 	void Start () {
