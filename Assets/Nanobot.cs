0879c8cadab3ae7b678814a46c39af54a414e05e
Marjie Volk <marjie.volk@gmail.com>
2015-04-19 09:57:44 -0700

    Added Nanobot.  You saw nothing

---
+++ Assets/Nanobot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nanobot : MonoBehaviour, TimestepManager.TimestepListener {
+
+    BulletGridGenerator currentLevel;
+    public NanobotSchematic schematic;
+
+	// Use this for initialization
+	void Start() {
+        currentLevel = FindObjectOfType<BulletGridGenerator>();
+        GameObject.FindObjectOfType<TimestepManager>().addListener(this);
+        schematic = GameObject.Instantiate(schematic);
+	}
+
+    public void notifyTimestep() {
+        currentLevel.getCellAt(gameObject.GetComponent<GridPositionComponent>().position).Cell.GetComponent<Cell>().Eat(1, false);
+        for (int x = 0; x < schematic.getTransformation().Length; x++) {
+            if (schematic.getTransformation()[x] != null) {
+                for (int y = 0; y < schematic.getTransformation()[x].Length; y++) {
+                    if (schematic.getTransformation()[x][y] != null) {
+                        currentLevel.moveBot(gameObject.GetComponent<GridPositionComponent>().position, schematic.getTransformation()[x][y], new GridPosition(x - 1, y - 1));
+                    }
+                }
+            }
+        }
+        GameObject.FindObjectOfType<TimestepManager>().removeListener(this);
+        GameObject.FindObjectOfType<TimestepManager>().destroyAtEnd(gameObject);
+    }
+}
