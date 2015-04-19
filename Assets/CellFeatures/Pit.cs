d131415d25acb07327e58e2596ac81cae9d7cd41
Marjie <marjie.volk@gmail.com>
2015-04-19 13:48:53 -0700

    Add pit implementation.

---
+++ Assets/CellFeatures/Pit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pit : CellFeature {
+
+    private BulletGridGenerator _grid;
+
+	// Use this for initialization
+	void Start () {
+        _grid = FindObjectOfType<BulletGridGenerator>();
+        NotifyResourceConsumed += ResourceConsumedHandler;
+	}
+
+    void ResourceConsumedHandler(int resourcesConsumed, bool exploded) {
+        _grid.DestroyNanobotAt(Cell.GridPosition);
+    }
+}
