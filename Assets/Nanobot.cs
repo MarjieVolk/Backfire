8c16df83b21db42d0d7babcf2f80291960e43bb3
Marjie <marjie.volk@gmail.com>
2015-04-19 13:45:09 -0700

    Handle exiting if bot is destroyed by consuming resources.

--- Assets/Nanobot.cs
+++ Assets/Nanobot.cs
@@ -16,12 +16,18 @@ public class Nanobot : MonoBehaviour, TimestepManager.TimestepListener {
 	}
 
     public void notifyTimestep() {
-        currentLevel.getCellAt(gameObject.GetComponent<GridPositionComponent>().position).Cell.GetComponent<Cell>().Eat(1, false);
+        GridPosition position = gameObject.GetComponent<GridPositionComponent>().position;
+        BulletGridGenerator.GameCell cell = currentLevel.getCellAt(position);
+        cell.Cell.GetComponent<Cell>().Eat(1, false);
+        if (cell.Nanobot == null) {
+            // TODO: Trigger animation for nanobot death.
+            return;
+        }
         for (int x = 0; x < schematic.getTransformation().Length; x++) {
             if (schematic.getTransformation()[x] != null) {
                 for (int y = 0; y < schematic.getTransformation()[x].Length; y++) {
                     if (schematic.getTransformation()[x][y] != null) {
-                        currentLevel.moveBotAnimated(gameObject.GetComponent<GridPositionComponent>().position, schematic.getTransformation()[x][y], new GridPosition(x - 1, y - 1), 5, false);
+                        currentLevel.moveBotAnimated(position, schematic.getTransformation()[x][y], new GridPosition(x - 1, y - 1), 5, false);
                     }
                 }
             }
