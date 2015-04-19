5c83d4e6d5d79f9ca1f1cca231f0ed86de35ba3d
Marjie <marjie.volk@gmail.com>
2015-04-18 20:27:48 -0700

    Re-add the schematic definition.

---
+++ Assets/NanobotSchematic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NanobotSchematic {
+
+    public NanobotSchematic[][] transformation;
+
+	// Use this for initialization
+	public NanobotSchematic () {
+        transformation = new NanobotSchematic[3][];
+        for(int x = 0; x < 3; x++) {
+            transformation[x] = new NanobotSchematic[3];
+        }
+    }
+
+    public void notifyTimestep() {
+    }
+}
