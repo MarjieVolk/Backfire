89db0f758ca97374f4b085a8feaac0eb9ca2cd9b
Marjie Volk <marjie.volk@gmail.com>
2015-04-19 14:18:30 -0700

    Added a main menu and LevelManager, which can be used to switch levels

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public string[] levels;
+    public string menu;
+
+    private int currentLevel = 0;
+    private bool inMenu = true;
+
+	void Start () {
+        DontDestroyOnLoad(this);
+	}
+	
+	void Update () {
+	    if (!inMenu && Input.GetKey(KeyCode.Escape)) {
+            returnToMenu();
+        }
+	}
+
+    public void advanceLevel() {
+        currentLevel++;
+        loadCurrentLevel();
+    }
+
+    public void returnToMenu() {
+        inMenu = true;
+        Application.LoadLevel(menu);
+    }
+
+    public void loadCurrentLevel() {
+        inMenu = false;
+        Application.LoadLevel(levels[currentLevel]);
+    }
+
+    public void exitGame() {
+        Application.Quit();
+    }
+}
