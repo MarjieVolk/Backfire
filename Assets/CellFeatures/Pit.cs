9c62a9296983bbaf23e5486a27684950f5d8ca51
Marjie <marjie.volk@gmail.com>
2015-04-19 18:03:08 -0700

    Add a warning message about pits to first levels tutorial messages.

--- Assets/CellFeatures/Pit.cs
+++ Assets/CellFeatures/Pit.cs
@@ -4,14 +4,17 @@ using System.Collections;
 public class Pit : CellFeature {
 
     private BulletGridGenerator _grid;
+    private TutorialTextManager text;
 
 	// Use this for initialization
 	void Start () {
         _grid = FindObjectOfType<BulletGridGenerator>();
+        text = FindObjectOfType<TutorialTextManager>();
         NotifyResourceConsumed += ResourceConsumedHandler;
 	}
 
     void ResourceConsumedHandler(int resourcesConsumed, bool exploded) {
+        text.pitEncountered = true;
         _grid.DestroyNanobotAt(Cell.GridPosition);
     }
 }
